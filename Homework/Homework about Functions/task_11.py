f576c8b6b348b3d70f1f004ce86350650a9f6d33
name <Romanzaitsev25@yandex.by>
2022-04-20 23:55:04 +0300

    Добавлена задача на рекурсию, Фебоначи

---
+++ Homework/Homework about Functions/task_11.py
@@ -0,0 +1,10 @@
+# Решение задачи фебоначи числа 7
+def feb(n):
+    if n == 1:
+        return 0
+    if n == 2:
+        return 1
+    return feb(n-1) + feb(n-2)
+
+if __name__ == '__main__':
+    print(feb(7))
