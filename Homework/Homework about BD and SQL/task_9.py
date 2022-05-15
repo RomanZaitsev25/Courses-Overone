30fa56a95c4c93a09ba5d62e74c6ccfdc03a3b49
name <Romanzaitsev25@yandex.by>
2022-05-15 16:14:42 +0300

    task completed

--- Homework/Homework about BD and SQL/task_9.py
+++ Homework/Homework about BD and SQL/task_9.py
@@ -7,3 +7,38 @@
 # Вывести  автора и название  книг, количество которых принадлежит интервалу
 # от 2 до 14 (включая границы). Информацию  отсортировать сначала по авторам
 # (в обратном алфавитном порядке), а затем по названиям книг (по алфавиту).
+
+import sqlite3
+
+conn = sqlite3.connect('name9.db')
+
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS book(
+    ID INTEGER PRIMARY KEY AUTOINCREMENT, 
+    title TEXT,
+    author TEXT, 
+    price INTEGER,
+    amount INTEGER
+) ''')
+
+cursor.execute(''' 
+INSERT INTO book(title,	author,	price, amount)
+VALUES ('Мастер и Маргарита', 'Булгаков М.А.', 670.99, 3),
+    ('Белая гвардия', 'Булгаков М.А.', 540.50, 5),
+    ('Идиот', 'Достоевский Ф.М.', 460.00, 10),
+    ('Братья Карамазовы', 'Достоевский Ф.М.', 799.01, 3),
+    ('Игрок', 'Достоевский Ф.М.', 480.50, 10),
+    ('Стихотворения и поэмы', 'Есенин С.А.', 650.00, 15)
+''')
+
+conn.commit()
+
+cursor.execute('''
+SELECT title, author FROM book
+WHERE amount BETWEEN 2 and 14
+ORDER by author DESC, title DESC;''')
+
+r = cursor.fetchall()
+print(r)
